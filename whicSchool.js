28b3f2c30923913c3489f4854297f9a632f2f213
Luis Bustos <luiscarlos.bustos1@gmail.com>
2021-09-14 14:24:30 -0400

    test whichSchool

---
+++ whicSchool.js
@@ -0,0 +1,17 @@
+const whichSchool = function(age){
+  if (age < 13){ 
+  
+   return ("Elementary School");
+  }
+  
+  else if (age >= 13 && age <= 18){ 
+  
+  return ("Secondary School");
+  }
+  
+  if  (age >=19) {
+  
+  return ("Lighthouse Labs");
+  
+    }
+  }
