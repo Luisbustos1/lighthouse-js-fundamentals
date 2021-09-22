31cafc0dfee9710b646e030f1f2bd164371e6760
Luis Bustos <luiscarlos.bustos1@gmail.com>
2021-09-21 22:10:18 -0400

    hello

---
+++ hello.js
@@ -0,0 +1,14 @@
+/*
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+*/
+
+const sayHello  = function (nombre) {
+  //nombre = "luis"
+  console.log("Hello, world, " + nombre);
+}
+
+sayHello();
